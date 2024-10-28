1af1f02c71a2446a352d804ee6cd75c4263ac33c
Stan van Baarsen <stanvanbaarsen@hotmail.com>
2024-10-28 00:53:07 +0000

    implement data streaming client-side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 type Message = {
@@ -8,14 +8,23 @@ type Message = {
 	isError?: boolean;
 }
 
-
 export default function MainPage() {
 	const [userInput, setUserInput] = useState('');
 	const [chatMessages, setChatMessages] = useState<Message[]>([]);
+	const [unfinishedMistralResponse, setUnfinishedMistralResponse] = useState('');
 	const [loading, setLoading] = useState(false);
+
+	const focusInput = () => {
+		document.getElementById('userInput')?.focus();
+	}
 
 	const sendMessage = async () => {
 		if (!userInput) return;
+
+		const messagesDiv = document.getElementById('messages');
+		if (messagesDiv) {
+			messagesDiv.scrollTop = messagesDiv.scrollHeight;
+		}
 
 		try {
 			const newMessages = [...chatMessages, {
@@ -25,10 +34,6 @@ export default function MainPage() {
 			setChatMessages(newMessages);
 			setUserInput('');
 			setLoading(true);
-			const messagesDiv = document.getElementById('messages');
-			if(messagesDiv) {
-				messagesDiv.scrollTop = messagesDiv.scrollHeight;
-			}
 
 			const res = await fetch('/api/mistral', {
 				method: 'POST',
@@ -38,36 +43,48 @@ export default function MainPage() {
 				body: JSON.stringify({ messages: newMessages }),
 			});
 
-			if(messagesDiv) {
+			if (!res.body) {
+				throw new Error('No response body');
+			}
+
+			const reader = res.body.getReader();
+			const decoder = new TextDecoder();
+			let message = '';
+
+			while (true) {
+				const { done, value } = await reader.read();
+				if (done) break;
+
+				message += decoder.decode(value, { stream: true });
+				setUnfinishedMistralResponse(message);
+			}
+			
+			setLoading(false);
+			setChatMessages((prev) => [...prev, {
+				content: message,
+				role: 'assistant',
+			}]);
+			setUnfinishedMistralResponse('');
+			if (messagesDiv) {
 				messagesDiv.scrollTop = messagesDiv.scrollHeight;
 			}
-
-			const data = await res.json();
-			setLoading(false);
-			if (data.error) {
-				setChatMessages((prev) => [...prev, {
-					content: data.error,
-					role: 'assistant',
-					isError: true,
-				}])
-			} else {
-				setChatMessages((prev) => [...prev, {
-					content: data.response,
-					role: 'assistant',
-				}]);
-			}
-
-			// allow user to type again
-			document.getElementById('userInput')?.focus();
-
+			focusInput();
+			
+			reader.cancel();
 		} catch (error) {
 			setChatMessages((prev) => [...prev, {
 				content: 'Error: ' + JSON.stringify(error),
 				role: 'assistant',
 				isError: true,
 			}]);
+			setLoading(false);
+			setUnfinishedMistralResponse('');
 		}
 	};
+
+	useEffect(() => {
+		focusInput();
+	}, []);
 
 	return (
 		<>
@@ -93,7 +110,11 @@ export default function MainPage() {
 							Mistral:
 						</b>
 						<br />
-						<span className="text-gray-500">Loading...</span>
+						{unfinishedMistralResponse ?
+							<ReactMarkdown>{unfinishedMistralResponse + ' ...'}</ReactMarkdown>
+							:
+							<span className="text-gray-500">Loading...</span>
+						}
 					</div>
 				}
 			</div>
@@ -104,14 +125,14 @@ export default function MainPage() {
 					value={userInput}
 					onChange={(e) => setUserInput(e.target.value)}
 					placeholder="Ask anything!"
-					onKeyUp={(e) => e.key === "Enter" && !loading ? sendMessage() : null}
+					onKeyUp={(e) => (e.key === "Enter" && !loading) ? sendMessage() : null}
 				/>
 				<button
 					disabled={loading}
 					onClick={sendMessage}
 					className="bg-black text-white rounded p-2"
 				>
-					Send
+					{loading ? 'Loading...' : 'Send'}
 				</button>
 			</div>
 		</>
